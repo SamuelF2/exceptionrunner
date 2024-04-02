72057e9265e69b6fa19fdf009568ee9a0dd7ced3
SamuelF2 <samueltfries@gmail.com>
2024-04-02 16:55:51 -0500

    Completed and fixed test cases

--- src/main/java/edu/baylor/ecs/si/Timer.java
+++ src/main/java/edu/baylor/ecs/si/Timer.java
@@ -29,7 +29,7 @@ public class Timer {
 	}
 	
 	public static long timeMe(long timeToWait) throws TimerException {
-		Long timeNow = null;
+		Long timeNow =0L;
 		try {
 		  if (timeToWait < 0) {
 			  throw new TimerException("Cannot be less than zero");
